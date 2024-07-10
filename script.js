3e0eea136675bbfcbd9c22fe5e22950298ef3a68
Snwqqq <stanislav.cards@gmail.com>
2024-07-10 18:49:42 +0300

    Create html,js,css file add basic structure

---
+++ script.js
@@ -0,0 +1,6 @@
+// there is 3x3 gird
+// thow players place their symbol at any place on the grid
+// first turn is player 1 he places his symbol
+// then player two place his
+// then reapeat until the is 3 symobls in a row.
+// 
